094602a9c4c49f476f8a5fde578853363bf0b1be
Daniil Miroshnikov <beywer2@yandex.ru>
2018-09-04 17:10:57 +0400

    cssnano fix

--- configs/postcss/postcss.config.js
+++ configs/postcss/postcss.config.js
@@ -2,14 +2,14 @@
 const env = process.env.NODE_ENV;
 
 const cssNanoOptions = {
-  // Because CssNano removes prefixes for less size. G -> loGic
-  autoprefixer: false
+  autoprefixer: false, // CssNano removes prefixes for less size. G -> loGic
+  mergeRules: false,
 };
 
 module.exports = {
   plugins: {
-    cssnano: env === "production" ? cssNanoOptions : false,
+    cssnano: env === 'production' ? cssNanoOptions : false,
     // Supported browsers defined in browserlist section in package.json file
-    autoprefixer: {}
-  }
+    autoprefixer: {},
+  },
 };
