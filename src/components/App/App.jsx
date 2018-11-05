7ef733cb17d7279cb39a2a084aa6062af8d0717a
Beywer <beywer2@yandex.ru>
2018-11-05 15:56:56 +0400

    ... some changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import styles from './App.css';
 
-const App = () => <div>React App component</div>;
+const App = () => <div className={styles.app}>React App component</div>;
 
 export default App;
