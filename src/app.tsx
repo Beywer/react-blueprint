79101a16491deb18ee93454b09b6f710da127b6e
Daniil Miroshnikov <beywer2@yadnex.ru>
2019-02-03 00:46:15 +0400

    tsconfig baseUrl

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
+import 'assets';
 import {App} from 'components/App';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import '../assets';
-import './styles/app.css';
+import 'styles/app.css';
 
 const root = document.getElementById('root');
 
