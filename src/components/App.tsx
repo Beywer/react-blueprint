2b12f6e1b8af20343432c7470b58053008471233
Daniil Beywer Miroshnikov <beywer2@yandex.ru>
2019-02-10 18:06:35 +0300

    Todo list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import {TodosList} from 'components/TodosList';
 import {ITodo} from 'domain/ITodo';
 import {IUser} from 'domain/IUser';
 import * as React from 'react';
-// import image from '../images/test.svg';
+import image from '../images/test.svg';
 import styles from '../styles/components/App.css';
 
 interface IAppProps {
@@ -21,7 +21,17 @@ export class App extends React.Component<IAppProps> {
         const {todos} = this.props;
 
         return (
-            <div className={styles.appRoot}>
+            <div>
+                <div className={styles.example}>
+                    <img
+                        className={styles.icon}
+                        src={image}
+                        alt="random svg icon"
+                    />
+                    <h1 className={styles.text}>
+                        Type Script Todo list
+                    </h1>
+                </div>
                 <TodosList todos={todos}/>
             </div>
         );
