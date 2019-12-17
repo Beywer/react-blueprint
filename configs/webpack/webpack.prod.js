22ff35f68513935968650ee868a977b8a8614034
Daniil Miroshnikov <beywer2@yandex.ru>
2019-12-17 23:59:53 +0400

    UglifyJS replaced by Terser

--- configs/webpack/webpack.prod.js
+++ configs/webpack/webpack.prod.js
@@ -1,16 +1,14 @@
 /* eslint-disable */
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = require('./webpack.base')({
     mode: 'production',
 
-    // disable builtin minification due to Terser error
-    // seems like it problem starts after 4.26 webpack ver
-    // (4.25 - no problem, 4.26 - not tested, but webpack changed minificator, 4.29 - problem)
     optimization: {
-        minimize: false,
+        minimize: true,
+        minimizer: [new TerserPlugin()]
     },
 
     // Only hashes for images and classes in production
@@ -24,8 +22,6 @@ module.exports = require('./webpack.base')({
             filename: "[name]_[hash].css",
             chunkFilename: "[id].css"
         }),
-        // Minify js
-        new UglifyJsPlugin(),
         // Remove previous build
         // For some reason 'cleanOnceBeforeBuildPatterns' does nothing
         new CleanWebpackPlugin({cleanAfterEveryBuildPatterns: ['./build'], verbose: true}),
